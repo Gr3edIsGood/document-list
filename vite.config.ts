9f1a8bbd5d52ec74fb0e6c02fce13c77dde01a38
Yaroslav <moccarrt@gmail.com>
2022-11-11 11:26:58 +0300

    dist added

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/list-search-drag-drop/',
+  base: '/document-list/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
